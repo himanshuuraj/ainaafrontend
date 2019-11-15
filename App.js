0688a84b7dae038593a8c6a55b5fa736eec56f90
himanshu <himanshu@mplgaming.com>
2019-11-15 12:11:37 +0530

    added changes ainaa

--- App.js
+++ App.js
@@ -1,19 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your appp!</Text>
-    </View>
-  );
+import { Scene, Router, Actions, Reducer } from "react-native-router-flux";
+
+import { Provider } from "react-redux";
+import store from "./store";
+import { Stack } from "react-native-router-flux";
+import LoginPage from "./src/pages/login";
+import RegisterationPage from "./src/pages/registeration";
+import SplashScreen from "./src/pages/splashScreen";
+import Home from "./src/pages/home";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+
+export default class App extends React.Component {
+
+  state = {
+    isReady : false
+  };
+
+  componentWillMount() {
+    this.loadFonts();
+  }
+
+  async loadFonts() {
+    await Font.loadAsync({
+      Roboto: require("./node_modules/native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("./node_modules/native-base/Fonts/Roboto_medium.ttf"),
+      //Ionicons: require('./node_modules/@expo/vector-icons/Ionicons.js')
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+    return (
+      <Provider store={store}>
+        <Router>
+        <Stack key="root">
+          <Scene
+            hideNavBar={true}
+            key="loginPage"
+            component={LoginPage}
+            title="LoginPage"
+          />
+          <Scene
+            hideNavBar={true}
+            key="home"
+            component={Home}
+            title="Home"
+          />
+          <Scene
+            hideNavBar={true}
+            key="splashScreen"
+            component={SplashScreen}
+            title="splashScreen"
+          />
+          <Scene
+            hideNavBar={true}
+            key="registerationPage"
+            component={RegisterationPage}
+            title="RegisterationPage"
+          />
+          </Stack>
+        </Router>
+      </Provider>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
